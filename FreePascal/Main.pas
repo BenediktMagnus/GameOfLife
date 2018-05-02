4a1d8a98f4edf1940404d5951984f40f9dac2902
Benedikt Magnus <gitkraken@magnuscraft.de>
2018-05-02 15:28:43 +0200

    Main paint routine.

--- FreePascal/Main.pas
+++ FreePascal/Main.pas
@@ -23,6 +23,7 @@ type
     Label_Y: TLabel;
     procedure Button_SetClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
+    procedure PaintBoxPaint(Sender: TObject);
   end;
 
 var
@@ -45,6 +46,34 @@ begin
   Grid := TGrid.Create;
 
   Grid.SetSize(Rows, Cols);
+end;
+
+procedure TMainForm.PaintBoxPaint(Sender: TObject);
+var
+  EntryHeight, EntryWidth: UInt16;
+  i, x, y: UInt16;
+begin
+  //Calculate row and col sizes:
+  EntryWidth := Round(PaintBox.Width / Rows);
+  EntryHeight := Round(PaintBox.Height / Cols);
+
+  //Paint row and col borders:
+  for i := 1 to Rows - 1 do
+  begin
+    x := EntryWidth * i - 1;
+    PaintBox.Canvas.Line(x, 0, x, PaintBox.Height);
+  end;
+  for i := 1 to Cols - 1 do
+  begin
+    y := EntryHeight * i - 1;
+    PaintBox.Canvas.Line(0, y, PaintBox.Width, y);
+  end;
+
+  //Paint entries:
+  for x := 0 to Rows - 1 do
+    for y := 0 to Cols - 1 do
+      if Grid.Field[x, y] then
+        PaintBox.Canvas.FillRect(EntryWidth * x, EntryHeight * y, EntryWidth * (x + 1) - 1, EntryHeight * (y + 1) - 1);
 end;
 procedure TMainForm.Button_SetClick(Sender: TObject);
 begin
